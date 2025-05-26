9bd4dd826d8c918d5d180d18a6a4d32d70922f86
francisAWJ <francisjinangkung@outlook.com>
2025-05-26 22:53:59 +0700

    reverted to `Navigator.push`

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -29,7 +29,7 @@ class LoginPage extends StatelessWidget {
             TextButton(
               onPressed: () {
                 // Navigate to register page
-                Navigator.pushReplacement(
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const RegisterPage()),
                 );
